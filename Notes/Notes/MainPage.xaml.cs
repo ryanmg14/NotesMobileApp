a666a6e29ae2b19e884b7fb26a259a049e361838
Ryan Gordon <ryanmg@bgsu.edu>
2020-08-04 19:23:45 -0400

    Created text editor and save/delete function

--- Notes/Notes/MainPage.xaml.cs
+++ Notes/Notes/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using Xamarin.Forms;
 
 namespace Notes
@@ -13,9 +14,30 @@ namespace Notes
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        readonly string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
         public MainPage()
         {
             InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
         }
     }
 }
