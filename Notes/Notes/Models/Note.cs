723f2e43a8b27390bc21c55fd8c48da845c0fd84
Ryan Gordon <ryanmg@bgsu.edu>
2020-08-05 19:31:35 -0400

    Added page that generates list of all notes saved

---
+++ Notes/Notes/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
