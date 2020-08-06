92bb1b419bc8840947aa638e34007c8662240fd3
Ryan Gordon <ryanmg@bgsu.edu>
2020-08-05 23:54:00 -0400

    App now creates local database w/ CRUD operations

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Notes.Data;
+using System;
 using System.IO;
 using Xamarin.Forms;
 
@@ -6,13 +7,38 @@ namespace Notes
 {
     public partial class App : Application
     {
-        public static string FolderPath { get; private set; }
+        static NoteDatabase database;
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
 
         public App()
         {
             InitializeComponent();
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
             MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
         }
         // ...
     }
